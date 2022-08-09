9cb857bbe2da620d86ae5bc8de4b90d5b311c407
furuya <furuya.to@uhmfg.co.jp>
2022-08-09 21:16:39 +0900

    struct basic

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	"fmt"
+)
+
+// 構造体 複数の値をひとまとめにした値
+// struct {
+// 	'''変数宣言'''
+
+// }
+
+// var 変数 struct { ...... }
+
+var myData struct {
+	Name string
+	Data []int
+}
+
 func main () {
-	
+	myData.Name = "Taro"
+	myData.Data = []int{10, 20, 30}
+	fmt.Println(myData)
 }
