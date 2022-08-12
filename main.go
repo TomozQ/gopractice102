44a99351669f63420c9a6ef30ef39e99459113cd
furuya <furuya.to@uhmfg.co.jp>
2022-08-12 22:06:35 +0900

    mthod

--- main.go
+++ main.go
@@ -4,33 +4,21 @@ import (
 	"fmt"
 )
 
-// 構造体 複数の値をひとまとめにした値
-// struct {
-// 	'''変数宣言'''
-
-// }
-
-// var 変数 struct { ...... }
-
-// var myData struct {
-// 	Name string
-// 	Data []int
-// }
-
-// 構造体をtypeで型として定義する
-// type 型名 struct {'''内容'''}
-// →　作成... 型名 {'''値の設定'''}
-
-// Mydata is structure 			「外部から利用可能」なもの（大文字で始まる名前）はコメントが必須
 type Mydata struct {
 	Name string
 	Data []int
 }
 
+func (md Mydata) PrintData() {	// (md Mydata)がレシーバーの指定
+	fmt.Println("*** Mydata ***")
+	fmt.Println("Name: ", md.Name)
+	fmt.Println("Data: ", md.Data)
+	fmt.Println("*** end ***")
+}
+
 func main () {
-	taro := new(Mydata)
-	fmt.Println(taro)
-	taro.Name = "Taro"
-	taro.Data = make([]int, 5, 5)
-	fmt.Println(taro)
+	taro := Mydata{
+		"Hanako", []int{98, 76, 54, 32, 10},
+	}
+	taro.PrintData()
 }
