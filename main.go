4a67e3dedd8c0467707c0bed8b61914888cf1979
furuya <furuya.to@uhmfg.co.jp>
2022-08-16 02:33:51 +0900

    go routin

--- main.go
+++ main.go
@@ -2,77 +2,38 @@ package main
 
 import (
 	"fmt"
-	"reflect"
+	"time"
 )
 
-// General is all type data.
-type General interface {}
-
-// GData is holding General value.
-type GData interface {
-	Set(nm string, g General) GData
-	Print()
-}
-
-// NData is structure.
-type NData struct {
-	Name string
-	Data []int
-}
-
-// Set is NData method.
-func (nd *NData) Set (nm string, g General) GData {
-	nd.Name = nm
-	// 引数gのKind型を調べる																	0が何型か
-	if reflect.TypeOf(g) == reflect.SliceOf(reflect.TypeOf(0)) {
-		nd.Data = g.([]int)
+func hello(s string, n int) {
+	for i := 1; i <= 10; i++ {
+		fmt.Printf("<%d %s>", i, s)
+		// nミリ秒だけ処理を停止させる。
+		time.Sleep(time.Duration(n) * time.Millisecond)
 	}
-	return nd
-}
-
-// Print is NData method
-func (nd *NData) Print () {
-	fmt.Printf("<<%s>>", nd.Name)
-	fmt.Println(nd.Data)
-}
-
-// SData is structure
-type SData struct {
-	Name string
-	Data []string
-}
-
-// Set is SData method.
-func (sd *SData) Set(nm string, g General) GData {
-	sd.Name = nm
-	// 																										""が何型か
-	if reflect.TypeOf(g) == reflect.SliceOf(reflect.TypeOf("")) {
-		sd.Data = g.([]string)
-	}
-	return sd
-}
-
-// Print is SData method.
-func (sd *SData) Print () {
-	fmt.Printf("* %s [%s] *\n", sd.Name, sd.Data)
 }
 
 func main () {
-	var data = []GData{}
-	data = append(data, new(NData).Set("Taro", 123))
-	data = append(data, new(SData).Set("Jiro", []string{"hello!", "bye"}))
-	data = append(data, new(NData).Set("Hanako", 98700))
-	data = append(data, new(SData).Set("Sachiko", "happy?"))
-	for _, ob := range data {
-		ob.Print()
-	}
+	go hello("hello", 50)
+	hello("bye!", 100)
 }
 
 /*
-	型アサーション
-	nd.Data = g.(int)
-	sd.Data = g.(string)
-	gはGeneral型だがこれによりint型やstring型としてDataに設定するようになっている。
+	出力結果
+	<1 bye!><1 hello><2 hello><2 bye!><3 hello><4 hello><3 bye!><5 hello><6 hello><4 bye!>...
 
-	空のインターフェイス型をうまく活用することで保持する値の異なる構造体を同一のインターフェイスとして扱えるようになる。
+	helloを50ミリ秒間隔で実行
+	byeを100ミリ秒間隔で実行
+*/
+
+/*
+	並行処理と非同期処理
+	「Goは文法レベルで並行処理を実現している」というのは、Goの大きな特徴の一つ
+	「そんなの他の言語でも普通にできるんじゃない？」と思うかもしれない。
+	例えばWebなどで多くの人に馴染みのあるJavaScriptはタイマー機能やAjaxといった機能で同時に複数の処理が行えるように見える。
+	が、実はこれは「並行処理」ではなく「非同期処理」。
+	同期せずに処理を実行できるということであり、実際に実行している処理は常に一つ。
+	順番に処理を実行しているところに「これをやって」と処理を割り込ませているだけ。
+	Goルーチンは、メインで実行される処理（スレッド）とは別に、独立したスレッドで処理を実行する。
+	非同期処理とは根本的に異なるもの
 */
