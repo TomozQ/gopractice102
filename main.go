2d8950e942a6093acddf0fcf86bba6d00f095510
furuya <furuya.to@uhmfg.co.jp>
2022-08-21 18:46:43 +0900

    exclusive processing thread

--- main.go
+++ main.go
@@ -3,84 +3,68 @@ package main
 import (
 	"time"
 	"fmt"
+	"sync"
+	"strconv"
 )
 
-/*
-	select {
-	case 文:
-		'''実行する処理'''
-	case 文:
-		'''実行する処理'''
-	
-	'''必要なだけcaseを用意'''
-
-	default:
-		'''全て当てはまらない場合の処理'''
-	}
-
-	caseにはチャンネル操作の文を用意する
-	基本的には「←チャンネル」という形でチャンネルから値を取り出す文を記述しておく。
-
-	selectに進むとチャンネルから値を取り出せたcaseに進む。
-	複数のチャンネルで値が取り出せる場合には、その中からランダムにcaseが選ばれる。
-	全てのチャンネルで準備が整わない場合には default文 が実行される
-*/
-
-func count(n int, s int, c chan int) {
-	for i := 1; i <= n; i++ {
-		c <- i
-		time.Sleep(time.Duration(s) * time.Millisecond)
-	}
+// SrData is structure.
+type SrData struct {
+	msg string
+	mux	sync.Mutex
 }
 
-/*
-	三つのスレッドでcount関数を実行
-*/
-func main() {
-	n1, n2, n3 := 3, 5, 10
-	m1, m2, m3 := 100, 75, 50
-	c1 := make(chan int)
-	go count(n1, m1, c1)
-	c2 := make(chan int)
-	go count(n2, m2, c2)
-	c3 := make(chan int)
-	go count(n3, m3, c3)
-
-	// 最後まで値が取り出せるようにn1,n2,n3を全て足した数分ループを回す
-	for i := 0; i < n1 + n2 + n3; i++ {
-		// チャンネルに値を送るとそのチャンネルのcaseにジャンプし、送られた値を変数reに取り出す。
-		select {
-		case re := <-c1:
-			fmt.Println("*   first  ", re)
-		case re := <-c2:
-			fmt.Println("**  second ", re)
-		case re := <-c3:
-			fmt.Println("*** third  ", re)
-		}
+func main () {
+	sd := SrData{msg: "Start"}	// Mutexは初期状態のままで、新たに値などを設定する必要はない。
+	prmsg := func(nm string, n int) {
+		fmt.Println(nm, sd.msg)
+		time.Sleep(time.Duration(n) * time.Millisecond)
 	}
 
-	fmt.Println("*** finish ***")
+	main := func(n int) {
+		const nm string = "*main"
+		sd.mux.Lock()
+		for i := 0; i < 5; i++ {
+			sd.msg += " m" + strconv.Itoa(i)
+			prmsg(nm, 100)
+		}
+		sd.mux.Unlock()
+	}
+
+	hello := func (n int) {
+		const nm string = "hello"
+		// sd.mux.Lock()
+		for i := 0; i < 5; i++ {
+			sd.msg += " h" + strconv.Itoa(i)
+			prmsg(nm, n)
+		}
+		sd.mux.Unlock()
+	}
+
+	go main(100)
+	go hello(50)
+	time.Sleep(3 * time.Second)
 }
 
 /*
 	出力結果
-	*** third   1
-	**  second  1
-	*   first   1
-	*** third   2
-	**  second  2
-	*   first   2
-	*** third   3
-	*** third   4
-	**  second  3
-	*   first   3
-	*** third   5
-	**  second  4
-	*** third   6
-	*** third   7
-	**  second  5
-	*** third   8
-	*** third   9
-	*** third   10
-	*** finish ***
+	hello Start h0
+	hello Start h0 h1
+	hello Start h0 h1 h2
+	hello Start h0 h1 h2 h3
+	hello Start h0 h1 h2 h3 h4
+	*main Start h0 h1 h2 h3 h4 m0
+	*main Start h0 h1 h2 h3 h4 m0 m1
+	*main Start h0 h1 h2 h3 h4 m0 m1 m2
+	*main Start h0 h1 h2 h3 h4 m0 m1 m2 m3
+	*main Start h0 h1 h2 h3 h4 m0 m1 m2 m3 m4
+*/
+
+/*
+	main関数,hello関数ともスレッドをロックしてから繰り返し処理を実行
+	繰り返しを抜けたらアンロック
+	他スレッドがsdにアクセスできなくなるため、もう一方の処理がアンロックされるまで待ち続けることになる。
+	
+	スレッドをロックすると、アンロックされるまでの間たスレッドはペンディング状態となる
+	別スレッドで実行している処理を全てロックしてしまうと、結局並行処理ではなく「複数のスレッドが順に実行される」という逐次処理になってしまう。
+	ロックは「この処理だけは外部からアクセスされては困る」という必要最低限の範囲に絞って行うようにする。
 */
