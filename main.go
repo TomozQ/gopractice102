fecd52f84e79812190dc7544b8043ad19231b4f5
furuya <furuya.to@uhmfg.co.jp>
2022-08-21 17:20:54 +0900

    two way channel value

--- main.go
+++ main.go
@@ -1,44 +1,39 @@
 package main
 
 import (
-	"time"
 	"fmt"
-	"strconv"
 )
 
-func prmsg(n int, s string) {
-	fmt.Println(s)
-	time.Sleep(time.Duration(n) * time.Millisecond)
-}
-
-/*
-	c <- s と実行して値を追加
-*/
-func first(n int, c chan string) {
-	const nm string = "first-"
-	for i := 0; i < 10; i++ {
-		s := nm + strconv.Itoa(i)
-		prmsg(n, s)
-		c <- s
+func total (cs chan int, cr chan int) {
+	n := <-cs
+	fmt.Println("n = ", n)
+	t := 0
+	for i := 1; i <= n; i++ {
+		t += i
 	}
-}
-
-/*
-	チャンネルcから取得した値を使ってprmsg関数を実行。
-*/
-func second(n int, c chan string) {
-	for i := 0; i < 10; i ++ {
-		prmsg(n, "second:[" + <-c + "]")
-	}
+	cr <- t
 }
 
 func main () {
-	// チャンネルを作成
-	c := make(chan string)
-	/*
-		firstとsecondに同じチャンネルcを渡す。（複数のスレッド間でチャンネルを利用する場合は、同じチャンネルを使う）
-	*/
-	go first(10, c)
-	second(10, c)
-	fmt.Println()
+	// チャンネルを二つ作成
+	cs := make(chan int)
+	cr := make(chan int)
+	// トータル関数にチャンネルを二つ渡す
+	go total(cs, cr)
+	cs <- 100
+	fmt.Println("total:", <- cr)
 }
+
+/*
+	出力結果
+	n =  100
+	total: 5050
+*/
+
+/*
+	total関数ではチャンネルからcsの値を取り出し、演算の結果をcrに追加
+	main関数ではチャンネルcsに値を追加し、チャンネルcrから取り出した値を表示
+	...csをmain→total
+	...crをtotal→main
+	というようにして、二つのチャンネルで双方向に値を送受していた。
+*/
